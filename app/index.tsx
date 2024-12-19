9e1b27e043b6df13748e777e7bab44f98690d154
TArch64 <tarasturchenko1551@gmail.com>
2024-12-19 20:48:10 +0200

    clean up

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from 'react';
+import { Text } from 'react-native';
+
+export default function Index(): ReactNode {
+    return (
+        <Text>
+            hello world
+        </Text>
+    )
+}
