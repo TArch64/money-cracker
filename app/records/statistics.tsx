e12d7e1d22705a356673bbedc74dff0ae1b47f37
TArch64 <tarasturchenko1551@gmail.com>
2025-01-04 21:24:53 +0200

    show title

--- app/records/statistics.tsx
+++ app/records/statistics.tsx
@@ -1,10 +1,24 @@
-import type { ReactNode } from 'react';
+import { type ReactNode, useMemo } from 'react';
 import { FullScreenLayout } from '@/components/layout';
 import { Text } from '@ui-kitten/components';
+import { useDateFormatter } from '@/hooks/formatters';
+import { MonthIdx } from '@/components/recordsList';
+import { useLocalSearchParams } from 'expo-router';
+
+type SearchParams = {
+  year: string;
+  month: string;
+};
 
 export default function Statistics(): ReactNode {
+  const { year, month } = useLocalSearchParams<SearchParams>();
+  const monthIdx = useMemo(() => new MonthIdx(+year, +month), [year, month]);
+
+  const dateFormatter = useDateFormatter({ year: 'numeric', month: 'long' });
+  const dateTitle = dateFormatter.format(monthIdx.date);
+
   return (
-    <FullScreenLayout title="Statistics">
+    <FullScreenLayout title={`${dateTitle} Statistics`}>
       <Text>statistics</Text>
     </FullScreenLayout>
   );
