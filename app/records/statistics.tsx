05b5c6ec075e82728e979fe82a69eb75fa2e0b9b
TArch64 <tarasturchenko1551@gmail.com>
2025-01-04 23:18:09 +0200

    add empty state

--- app/records/statistics.tsx
+++ app/records/statistics.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, useMemo } from 'react';
+import { type PropsWithChildren, type ReactNode, useMemo } from 'react';
 import { FullScreenLayout } from '@/components/layout';
 import { useDateFormatter, useMoneyFormatter } from '@/hooks/formatters';
 import { MonthIdx } from '@/components/recordsList';
@@ -8,18 +8,36 @@ import { StyleSheet, View, type ViewStyle } from 'react-native';
 import { RecordType } from '@/enums';
 import { groupBy } from 'lodash-es';
 import { Text } from '@ui-kitten/components';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import type { Record } from '@/db';
 
 type SearchParams = {
   year: string;
   month: string;
 };
 
+interface IWrapperProps extends PropsWithChildren {
+  monthIdx: MonthIdx;
+}
+
+function Wrapper(props: IWrapperProps): ReactNode {
+  const dateFormatter = useDateFormatter({ year: 'numeric', month: 'long' });
+  const dateTitle = dateFormatter.format(props.monthIdx.date);
+
+  return (
+    <FullScreenLayout title={`${dateTitle} Statistics`}>
+      {props.children}
+    </FullScreenLayout>
+  );
+}
+
+function sumRecords(records: Record[]): number {
+  return records.reduce((acc, record) => acc + record.value, 0);
+}
+
 export default function Statistics(): ReactNode {
   const { year, month } = useLocalSearchParams<SearchParams>();
   const monthIdx = useMemo(() => new MonthIdx(+year, +month), [year, month]);
-
-  const dateFormatter = useDateFormatter({ year: 'numeric', month: 'long' });
-  const dateTitle = dateFormatter.format(monthIdx.date);
 
   const recordsQuery = useRecordsMonthSuspenseQuery({
     year: monthIdx.year,
@@ -30,35 +48,47 @@ export default function Statistics(): ReactNode {
       type: RecordType.EXPENSE,
     },
 
-    select: (records) => ({
-      categories: groupBy(records, 'category.name'),
-      total: records.reduce((acc, record) => acc + record.value, 0),
-    }),
+    select(records) {
+      return {
+        categoriesPie: Object.entries(groupBy(records, 'category.name')).map(([name, records]) => ({
+          x: name,
+          y: sumRecords(records),
+        })),
+
+        total: sumRecords(records),
+      };
+    },
   });
 
   const moneyFormatter = useMoneyFormatter();
   const total = moneyFormatter.format(recordsQuery.data.total);
 
+  if (!recordsQuery.data.total) {
+    return (
+      <Wrapper monthIdx={monthIdx}>
+        <SafeAreaView style={[styles.column, styles.emptyColumn]} edges={['bottom']}>
+          <Text>
+            No expenses recorded yet
+          </Text>
+        </SafeAreaView>
+      </Wrapper>
+    );
+  }
+
   return (
-    <FullScreenLayout title={`${dateTitle} Statistics`}>
+    <Wrapper monthIdx={monthIdx}>
       <View style={styles.column}>
         <View style={styles.totalsRow}>
           <Text category="s1">
             Total Expenses
           </Text>
 
-          {recordsQuery.data.total ? (
-            <Text status="danger">
-              - {total}
-            </Text>
-          ) : (
-            <Text>
-              None
-            </Text>
-          )}
+          <Text status="danger">
+            - {total}
+          </Text>
         </View>
       </View>
-    </FullScreenLayout>
+    </Wrapper>
   );
 }
 
@@ -69,6 +99,12 @@ const styles = StyleSheet.create({
     gap: 16,
     paddingTop: 12,
     paddingHorizontal: 20,
+  } satisfies ViewStyle,
+
+  emptyColumn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
   } satisfies ViewStyle,
 
   totalsRow: {
