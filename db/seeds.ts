f0fdfac9a99af7d85bc4be4059ff20909c2a0785
TArch64 <tarasturchenko1551@gmail.com>
2025-02-15 20:59:05 +0200

    update seeds

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,30 +1,26 @@
+import { getDaysInMonth, setDate } from 'date-fns';
 import type { AppDatabase } from '@/db/Provider';
 import { IntroState, RecordType } from '@/enums';
 import { MonthIdx } from '@/stores';
-import { categories, type RecordInsert, records, users } from './schema';
+import {
+  budgetCategories,
+  type BudgetCategoryInsert,
+  budgets,
+  categories,
+  type RecordInsert,
+  records,
+  users,
+} from './schema';
+import seedsData from './seeds.json';
 
 function getDateRange(): MonthIdx[] {
-  const currentIdx = MonthIdx.current();
-  const range = [currentIdx];
+  const range = [MonthIdx.current()];
 
-  for (let index = 0; index < 10; index++) {
+  for (let index = 0; index < 2; index++) {
     range.unshift(range[0].previous);
   }
 
   return range;
-}
-
-function getRecordValues(): number[] {
-  const count = Math.floor(Math.random() * 10) + 5;
-  return new Array(count).fill(0).map(() => 100 + Math.floor(Math.random() * 2_000));
-}
-
-function getMonthDays(idx: MonthIdx): Date[] {
-  const lastDay = new Date(idx.date.getFullYear(), idx.date.getMonth(), 0).getDate();
-
-  return new Array(lastDay)
-    .fill(0)
-    .map((_, index) => new Date(idx.date.getFullYear(), idx.date.getMonth(), index + 1));
 }
 
 export async function runSeeds(db: AppDatabase): Promise<void> {
@@ -38,33 +34,54 @@ export async function runSeeds(db: AppDatabase): Promise<void> {
     const [incomeCategory, ...expenseCategories] = await tx.insert(categories)
       .values([
         { type: RecordType.INCOME, name: 'Зарплата' },
-        { type: RecordType.EXPENSE, name: 'Продукти' },
+        { type: RecordType.EXPENSE, name: 'Їжа' },
         { type: RecordType.EXPENSE, name: 'Транспорт' },
         { type: RecordType.EXPENSE, name: 'Розваги' },
         { type: RecordType.EXPENSE, name: 'Одяг' },
         { type: RecordType.EXPENSE, name: 'Кредити' },
         { type: RecordType.EXPENSE, name: 'Донати' },
+        { type: RecordType.EXPENSE, name: 'Здоровʼя' },
+        { type: RecordType.EXPENSE, name: 'Житло' },
         { type: RecordType.EXPENSE, name: 'Інше' },
       ])
-      .returning({ id: categories.id });
+      .returning();
 
-    await tx.insert(records).values([
-      ...monthIdxes.map((idx): RecordInsert => ({
+    const expenseCategoriesMap = Object.fromEntries(expenseCategories.map((category) => [category.name, category]));
+
+    await tx.insert(records).values(monthIdxes.flatMap((monthIdx, index): RecordInsert[] => [
+      {
         categoryId: incomeCategory.id,
         type: RecordType.INCOME,
-        date: idx.date,
-        value: 350_000,
-      })),
+        date: setDate(monthIdx.date, 5),
+        value: 130_000,
+      },
+      ...seedsData.expenses[index]
+        .filter((data) => getDaysInMonth(monthIdx.date) > data.day)
+        .map((data): RecordInsert => ({
+          categoryId: expenseCategoriesMap[data.category].id,
+          type: RecordType.EXPENSE,
+          date: setDate(monthIdx.date, data.day),
+          value: data.value,
+        })),
+    ]));
 
-      ...monthIdxes.flatMap((idx) =>
-        getMonthDays(idx).flatMap((date) =>
-          getRecordValues().flatMap((value): RecordInsert => ({
-            categoryId: expenseCategories[Math.floor(Math.random() * expenseCategories.length)].id,
-            type: RecordType.EXPENSE,
-            date,
-            value,
-          })))),
-    ]);
+    const budgets_ = await tx
+      .insert(budgets)
+      .values(monthIdxes.map((monthIdx) => ({
+        year: monthIdx.year,
+        month: monthIdx.month,
+      })))
+      .returning();
+
+    await tx
+      .insert(budgetCategories)
+      .values(budgets_.flatMap((budget) => {
+        return seedsData.goals.map((data): BudgetCategoryInsert => ({
+          categoryId: expenseCategoriesMap[data.category].id,
+          budgetId: budget.id,
+          goal: data.goal,
+        }));
+      }));
 
     await tx
       .update(users)
